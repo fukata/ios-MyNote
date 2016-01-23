e6eb8dd4146a5e84cff3edea614bf6c2004faeee
fukata <tatsuya.fukata@gmail.com>
2016-01-23 15:36:24 +0700

    Initial Commit

---
+++ MyNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyNote
+//
+//  Created by Tatsuya Fukata on 1/23/16.
+//  Copyright © 2016 fukata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyNote
+//
+//  Created by Tatsuya Fukata on 1/23/16.
+//  Copyright © 2016 fukata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
